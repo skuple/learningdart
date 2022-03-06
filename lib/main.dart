19c8d72559b5aecd088d80bafe8a4bd6f190539e
Rahendra Prabandaru <randarumoklet20@gmail.com>
2022-03-06 14:22:37 +0700

    AppBar Widget add

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,19 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
-    ),);
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
